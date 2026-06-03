7ee017039db493bfbf47096c9a1e140bd9b673ed
n1520369 <n1520536@gmail.com>
2026-06-03 13:50:55 +0530

    Create sample_code.py

---
+++ sample_code.py
@@ -0,0 +1,13 @@
+# Sample Python Program
+
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+result1 = add(5, 10)
+result2 = multiply(4, 6)
+
+print(result1)
+print(result2)
